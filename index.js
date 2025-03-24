cd5536f531311d4929f49d5299f58a2c7ad639ff
Dibyajyoti Pramanick <dibyajyotipramanick0750@gmail.com>
2025-03-24 10:26:46 +0530

    Add server side error handling

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Wallpaper = require("./models/wallpaper");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wallpaperswebsite";
 
@@ -35,10 +37,13 @@ app.get("/", (req, res) => {
 });
 
 // Index
-app.get("/wallpapers", async (req, res) => {
-  const wallpapers = await Wallpaper.find({});
-  res.render("wallpapers/index", { wallpapers });
-});
+app.get(
+  "/wallpapers",
+  wrapAsync(async (req, res) => {
+    const wallpapers = await Wallpaper.find({});
+    res.render("wallpapers/index", { wallpapers });
+  })
+);
 
 // New
 app.get("/wallpapers/new", (req, res) => {
@@ -46,66 +51,109 @@ app.get("/wallpapers/new", (req, res) => {
 });
 
 // Create
-app.post("/wallpapers", async (req, res) => {
-  const { image, title, description, category, tags } = req.body.wallpaper;
+app.post(
+  "/wallpapers",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.wallpaper) {
+      throw new ExpressError(400, "Wallpaper data is required!");
+    }
 
-  const newWallpaper = new Wallpaper({
-    image,
-    title,
-    description,
-    category,
-    tags: tags.split(",").map((tag) => tag.trim()),
-  });
-
-  const savedWallpaper = await newWallpaper.save();
-  console.log(savedWallpaper);
-
-  res.redirect("/wallpapers");
-});
-
-// Show
-app.get("/wallpapers/:id", async (req, res) => {
-  const { id } = req.params;
-  const wallpaper = await Wallpaper.findById(id);
-  res.render("wallpapers/show", { wallpaper });
-});
-
-// Edit
-app.get("/wallpapers/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const wallpaper = await Wallpaper.findById(id);
-  res.render("wallpapers/edit", { wallpaper });
-});
-
-// Update
-app.put("/wallpapers/:id", async (req, res) => {
-  const { id } = req.params;
-  const { image, title, description, category, tags } = req.body.wallpaper;
-  const formattedTags = tags.split(",").map((tag) => tag.trim());
-
-  const updatedWallpaper = await Wallpaper.findByIdAndUpdate(
-    id,
-    {
+    const { image, title, description, category, tags } = req.body.wallpaper;
+    const newWallpaper = new Wallpaper({
       image,
       title,
       description,
       category,
-      tags: formattedTags,
-    },
-    { new: true }
-  );
-  console.log(updatedWallpaper);
+      tags: tags.split(",").map((tag) => tag.trim()),
+    });
 
-  res.redirect("/wallpapers");
-});
+    const savedWallpaper = await newWallpaper.save();
+    console.log(savedWallpaper);
+
+    res.redirect("/wallpapers");
+  })
+);
+
+// Show
+app.get(
+  "/wallpapers/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const wallpaper = await Wallpaper.findById(id);
+
+    if (!wallpaper) {
+      throw new ExpressError(404, "Wallpaper not found!");
+    }
+
+    res.render("wallpapers/show", { wallpaper });
+  })
+);
+
+// Edit
+app.get(
+  "/wallpapers/:id/edit",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const wallpaper = await Wallpaper.findById(id);
+
+    if (!wallpaper) {
+      throw new ExpressError(404, "Wallpaper not found!");
+    }
+
+    res.render("wallpapers/edit", { wallpaper });
+  })
+);
+
+// Update
+app.put(
+  "/wallpapers/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.wallpaper) {
+      throw new ExpressError(400, "Wallpaper data is required!");
+    }
+
+    const { id } = req.params;
+    const { image, title, description, category, tags } = req.body.wallpaper;
+    const formattedTags = tags.split(",").map((tag) => tag.trim());
+    const updatedWallpaper = await Wallpaper.findByIdAndUpdate(
+      id,
+      {
+        image,
+        title,
+        description,
+        category,
+        tags: formattedTags,
+      },
+      { new: true }
+    );
+    console.log(updatedWallpaper);
+    res.redirect("/wallpapers");
+  })
+);
 
 // Destroy
-app.delete("/wallpapers/:id", async (req, res) => {
-  const { id } = req.params;
-  let deletedWall = await Wallpaper.findByIdAndDelete(id);
-  console.log(deletedWall);
+app.delete(
+  "/wallpapers/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let deletedWall = await Wallpaper.findByIdAndDelete(id);
 
-  res.redirect("/wallpapers");
+    if (!deletedWall) {
+      throw new ExpressError(404, "Wallpaper not found!");
+    }
+
+    console.log(deletedWall);
+    res.redirect("/wallpapers");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(port, () => {
