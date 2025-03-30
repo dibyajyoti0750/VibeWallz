9b670af97da9b1a3b61efe54d789403cd193401c
Dibyajyoti Pramanick <dibyajyotipramanick0750@gmail.com>
2025-03-30 17:44:45 +0530

    Add MVC pattern for wallpapers

---
+++ controllers/wallpapers.js
@@ -0,0 +1,110 @@
+const Wallpaper = require("../models/wallpaper");
+
+module.exports.index = async (req, res) => {
+  const wallpapers = await Wallpaper.find({}).sort({ createdAt: -1 }); // Sorts in descending order (newest first)
+  res.render("wallpapers/index", { wallpapers });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("wallpapers/new", { hideFooter: true });
+};
+
+module.exports.showWallpaper = async (req, res) => {
+  const { id } = req.params;
+  const wallpaper = await Wallpaper.findById(id)
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("owner");
+
+  if (!wallpaper) {
+    req.flash("error", "The wallpaper you're looking for doesn't exist!");
+    return res.redirect("/wallpapers");
+  }
+
+  res.render("wallpapers/show", { wallpaper });
+};
+
+module.exports.uploadWallpaper = async (req, res, next) => {
+  const { wallpaper } = req.body;
+
+  let setIsFree = wallpaper.isFree === undefined ? false : true;
+
+  let formattedTags = [];
+  if (typeof wallpaper.tags === "string") {
+    formattedTags = wallpaper.tags
+      .split(",")
+      .map((tag) => tag.trim().toLowerCase())
+      .filter((tag, index, self) => tag && self.indexOf(tag) === index); // Remove empty & duplicate tags
+  }
+
+  const newWallpaper = new Wallpaper({
+    ...wallpaper, // Spread all properties
+    tags: formattedTags, // Overwrite tags with formatted ones
+    isFree: setIsFree,
+    owner: req.user._id,
+  });
+
+  await newWallpaper.save();
+
+  req.flash("success", "Wallpaper uploaded successfully!");
+  res.redirect("/wallpapers");
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const { id } = req.params;
+  const wallpaper = await Wallpaper.findById(id);
+
+  if (!wallpaper) {
+    req.flash("error", "The wallpaper you're looking for doesn't exist!");
+    return res.redirect("/wallpapers");
+  }
+
+  res.render("wallpapers/edit", { wallpaper, hideFooter: true });
+};
+
+module.exports.updateWallpaper = async (req, res) => {
+  const { id } = req.params;
+  const { wallpaper } = req.body;
+
+  let setIsFree = wallpaper.isFree === undefined ? false : true;
+
+  let formattedTags = [];
+  if (typeof wallpaper.tags === "string") {
+    formattedTags = wallpaper.tags
+      .split(",")
+      .map((tag) => tag.trim().toLowerCase())
+      .filter((tag, index, self) => tag !== "" && self.indexOf(tag) === index);
+  }
+
+  const updatedWallpaper = await Wallpaper.findByIdAndUpdate(
+    id,
+    {
+      ...wallpaper,
+      tags: formattedTags,
+      isFree: setIsFree,
+    },
+    { new: true } // Return the updated document
+  );
+
+  console.log(updatedWallpaper);
+  req.flash("success", "Wallpaper updated successfully!");
+  res.redirect(`/wallpapers/${id}`);
+};
+
+module.exports.deleteWallpaper = async (req, res) => {
+  const { id } = req.params;
+  let deletingWall = await Wallpaper.findByIdAndDelete(id);
+
+  if (!deletingWall) {
+    req.flash("error", "The wallpaper you're looking for doesn't exist!");
+    return res.redirect("/wallpapers");
+  }
+
+  console.log(deletingWall);
+  req.flash("deleted", "Wallpaper deleted successfully!");
+  res.redirect("/wallpapers");
+};
--- routes/wallpaper.js
+++ routes/wallpaper.js
@@ -3,150 +3,41 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const Wallpaper = require("../models/wallpaper");
 const { isLoggedIn, isOwner, validateWallpaper } = require("../middleware");
+const wallpaperController = require("../controllers/wallpapers");
 
-// Index
-router.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const wallpapers = await Wallpaper.find({}).sort({ createdAt: -1 }); // Sorts in descending order (newest first)
-    res.render("wallpapers/index", { wallpapers });
-  })
-);
+router.get("/", wrapAsync(wallpaperController.index));
 
-// New
-router.get("/new", isLoggedIn, (req, res) => {
-  res.render("wallpapers/new", { hideFooter: true });
-});
+router.get("/new", isLoggedIn, wallpaperController.renderNewForm);
 
-// Create
 router.post(
   "/",
   isLoggedIn,
   validateWallpaper,
-  wrapAsync(async (req, res, next) => {
-    const { wallpaper } = req.body;
-
-    let setIsFree = wallpaper.isFree === undefined ? false : true;
-
-    let formattedTags = [];
-    if (typeof wallpaper.tags === "string") {
-      formattedTags = wallpaper.tags
-        .split(",")
-        .map((tag) => tag.trim().toLowerCase())
-        .filter((tag, index, self) => tag && self.indexOf(tag) === index); // Remove empty & duplicate tags
-    }
-
-    const newWallpaper = new Wallpaper({
-      ...wallpaper, // Spread all properties
-      tags: formattedTags, // Overwrite tags with formatted ones
-      isFree: setIsFree,
-      owner: req.user._id,
-    });
-
-    await newWallpaper.save();
-
-    req.flash("success", "Wallpaper uploaded successfully!");
-    res.redirect("/wallpapers");
-  })
+  wrapAsync(wallpaperController.uploadWallpaper)
 );
 
-// Show
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const wallpaper = await Wallpaper.findById(id)
-      .populate({
-        path: "comments",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate("owner");
+router.get("/:id", wrapAsync(wallpaperController.showWallpaper));
 
-    if (!wallpaper) {
-      req.flash("error", "The wallpaper you're looking for doesn't exist!");
-      return res.redirect("/wallpapers");
-    }
-
-    res.render("wallpapers/show", { wallpaper });
-  })
-);
-
-// Edit
 router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const wallpaper = await Wallpaper.findById(id);
-
-    if (!wallpaper) {
-      req.flash("error", "The wallpaper you're looking for doesn't exist!");
-      return res.redirect("/wallpapers");
-    }
-
-    res.render("wallpapers/edit", { wallpaper, hideFooter: true });
-  })
+  wrapAsync(wallpaperController.renderEditForm)
 );
 
-// Update
 router.put(
   "/:id",
   isLoggedIn,
   isOwner,
   validateWallpaper,
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const { wallpaper } = req.body;
-
-    let setIsFree = wallpaper.isFree === undefined ? false : true;
-
-    let formattedTags = [];
-    if (typeof wallpaper.tags === "string") {
-      formattedTags = wallpaper.tags
-        .split(",")
-        .map((tag) => tag.trim().toLowerCase())
-        .filter(
-          (tag, index, self) => tag !== "" && self.indexOf(tag) === index
-        );
-    }
-
-    const updatedWallpaper = await Wallpaper.findByIdAndUpdate(
-      id,
-      {
-        ...wallpaper,
-        tags: formattedTags,
-        isFree: setIsFree,
-      },
-      { new: true } // Return the updated document
-    );
-
-    console.log(updatedWallpaper);
-    req.flash("success", "Wallpaper updated successfully!");
-    res.redirect(`/wallpapers/${id}`);
-  })
+  wrapAsync(wallpaperController.updateWallpaper)
 );
 
-// Destroy
 router.delete(
   "/:id",
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    let deletingWall = await Wallpaper.findByIdAndDelete(id);
-
-    if (!deletingWall) {
-      req.flash("error", "The wallpaper you're looking for doesn't exist!");
-      return res.redirect("/wallpapers");
-    }
-
-    console.log(deletingWall);
-    req.flash("deleted", "Wallpaper deleted successfully!");
-    res.redirect("/wallpapers");
-  })
+  wrapAsync(wallpaperController.deleteWallpaper)
 );
 
 module.exports = router;
