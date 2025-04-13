83861b258eeecd9720b321e8660668118d81a384
Dibyajyoti Pramanick <dibyajyotipramanick0750@gmail.com>
2025-04-13 22:06:49 +0530

    Set up cloud img storing

--- routes/wallpaper.js
+++ routes/wallpaper.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const cloudinary = require("../cloudConfig");
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateWallpaper } = require("../middleware");
 const wallpaperController = require("../controllers/wallpapers");
@@ -7,11 +11,21 @@ const wallpaperController = require("../controllers/wallpapers");
 router
   .route("/")
   .get(wrapAsync(wallpaperController.index))
-  .post(
-    isLoggedIn,
-    validateWallpaper,
-    wrapAsync(wallpaperController.uploadWallpaper)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateWallpaper,
+  //   wrapAsync(wallpaperController.uploadWallpaper)
+  // );
+  .post(upload.single("wallpaper[image]"), async (req, res) => {
+    const file = req.file;
+    const b64 = Buffer.from(file.buffer).toString("base64");
+    const dataURI = `data:${file.mimetype};base64,${b64}`;
+    const result = await cloudinary.uploader.upload(dataURI, {
+      folder: "VibeWallz",
+    });
+
+    res.send(result);
+  });
 
 router.post(
   "/:id/like",
