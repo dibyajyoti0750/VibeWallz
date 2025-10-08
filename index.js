22e323de4a7e4ca5e8f1c0f1c23f1750192e09da
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2025-10-08 23:00:05 +0530

    typo

--- index.js
+++ index.js
@@ -83,7 +83,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/callback", // "https://vibewallz-ai.onrender.com/auth/google/callback",
+      callbackURL: "https://vibewallz-ai.onrender.com/auth/google/callback",
+      // "http://localhost:8080/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
